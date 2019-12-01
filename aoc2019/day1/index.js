7e7d0eec40c0b51e7f22394e4effcb32fffe3daa
Niels Bokmans <niels@bluenotion.nl>
2019-12-01 22:42:46 +0100

    AoC day1

---
+++ aoc2019/day1/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const readline = require('readline')
+
+fuelRequired = (mass) => Math.floor(Number(mass) / 3) - 2
+fuelRequiredP2 = (mass, accum = 0, idx = 0) => {
+    const fuelForMass = fuelRequired(mass);
+
+    if (fuelForMass <= 0) {
+        return accum;
+    }
+
+    accum += fuelForMass;
+
+    return fuelRequiredP2(fuelForMass, accum, ++idx);
+}
+
+//Tests p1
+console.log(fuelRequired(12))
+console.log(fuelRequired(14))
+console.log(fuelRequired(1969))
+console.log(fuelRequired(100756))
+
+//Tests p2
+console.log(fuelRequiredP2(12))
+console.log(fuelRequiredP2(14))
+console.log(fuelRequiredP2(1969))
+console.log(fuelRequiredP2(100756))
+
+let sum = 0;
+let sumP2 = 0;
+
+const fileStream = fs.createReadStream('input.txt');
+
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+    sum += fuelRequired(line);
+    sumP2 += fuelRequiredP2(line);
+});
+
+
+rl.on('close', () => {
+    console.log(`P1: ${sum}`);
+    console.log(`P2: ${sumP2}`)
+})
